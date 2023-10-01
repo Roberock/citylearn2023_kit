397b4ccfcc2217e274e2a02b2107962903aa9126
Roberto Rocchetta <roberto.rocchetta88@gmail.com>
2023-10-01 10:06:01 +0200

    submission agent - PPO_mix_agent_week_hour_feature

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -5,9 +5,9 @@ from agents.ppo_agent import PPO_comfort_agent, PPO_mix_agent_530, PPO_mix_agent
 #####                Specify your agent here                  #####
 ###################################################################
 
-SubmissionAgent = BasicRBCAgent
+# SubmissionAgent = BasicRBCAgent
 
 # SubmissionAgent = PPO_comfort_agent
 # SubmissionAgent = PPO_mix_agent
 # SubmissionAgent = PPO_mix_agent_530
-# SubmissionAgent = PPO_mix_agent_week_hour_feature
+SubmissionAgent = PPO_mix_agent_week_hour_feature
